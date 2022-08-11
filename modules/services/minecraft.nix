196822e5b522446f8e2293b0ecb012ecaed47880
BattleCh1cken <BattleCh1cken@Larkov.de>
2022-08-11 08:52:19 -0400

    added minecraft backup service to minecraft module

--- modules/services/minecraft.nix
+++ modules/services/minecraft.nix
@@ -26,12 +26,38 @@ in
           -e VERSION="1.18.2" \
           -e PACKWIZ_URL="https://battlech1cken.github.io/Quilt-Modpack/pack/.minecraft/pack.toml" \
           -e MOTD="powered by imbeciles" \
-          -e ENABLE_RCON="true" \
-          -e RCON_PORT="28016" \
           --rm itzg/minecraft-server
+          '';
+        ExecStop = ''
+          ${pkgs.docker}/bin/docker stop mc
         '';
-        ExecStop = ''${pkgs.docker}/bin/docker stop mc'';
         # Restart = "always";
+      };
+    };
+    systemd.services.mc-backup = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "docker.service" "docker.socket" ];
+      requires = [ "docker.service" "docker.socket" ];
+      serviceConfig = {
+        Type = "simple";
+        User = "battlechicken";
+        ExecStart = ''
+          ${pkgs.docker}/bin/docker run \
+          --name mc-backup \
+          --network container:mc \
+          -v /home/battlechicken/data/backups:/data \
+          -v /home/battlechicken/data/minecraft \
+          -e BACKUP_METHOD="restic" \
+          -e INITIAL_DELAY="1m" \
+          -e RESTIC_PASSWORD="password" \
+          -e RESTIC_REPOSITORY="/home/battlechicken/data/backups" \
+          -e PRUNE_RESTIC_RETENTION="--keep-daily 7 --keep-weekly 5" \
+          --rm itzg/mc-backup
+        '';
+        ExecStop = ''
+          ${pkgs.docker}/bin/docker stop mc-backup
+        '';
+        Restart = "always";
       };
     };
     networking.firewall.allowedTCPPorts = [ 25565 ];
