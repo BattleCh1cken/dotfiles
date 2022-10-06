d46cb4d940caea093dd56f8eb20aaeb5f8e4a016
BattleCh1cken <BattleCh1cken@Larkov.de>
2022-10-06 08:33:31 -0400

    feat: applied status2d patch to dwm

--- modules/home/desktop/dwm/scripts/bar.sh
+++ modules/home/desktop/dwm/scripts/bar.sh
@@ -1,54 +1,69 @@
 #!/bin/dash
 
+# ^c$var^ = fg color
+# ^b$var^ = bg color
+
 interval=0
 
+# colors
+black=#1E1D2D
+green=#ABE9B3
+white=#D9E0EE
+grey=#282737
+blue=#96CDFB
+red=#F28FAD
+darkblue=#83bae8
 
 cpu() {
-	cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
-  printf "cpu: %s" "$cpu_val"
+  cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
+
+  printf "^c$black^ ^b$green^ CPU"
+  printf "^c$white^ ^b$grey^ $cpu_val"
 }
 
-#pkg_updates() {
-  #updates=""
-	#updates=$(doas xbps-install -un | wc -l) # void
-	# updates=$(checkupdates | wc -l)   # arch
-	# updates=$(aptitude search '~U' | wc -l)  # apt (ubuntu,debian etc)
+pkg_updates() {
+  updates=$(doas xbps-install -un | wc -l) # void
+  # updates=$(checkupdates | wc -l)   # arch
+  # updates=$(aptitude search '~U' | wc -l)  # apt (ubuntu,debian etc)
 
-	#if [ -z "$updates" ]; then
-		#printf "  ^c$green^    Fully Updated"
-	#else
-		#printf "  ^c$green^    $updates"" updates"
-	#fi
-#}
+  if [ -z "$updates" ]; then
+    printf "  ^c$green^    Fully Updated"
+  else
+    printf "  ^c$green^    $updates"" updates"
+  fi
+}
 
 battery() {
-	get_capacity="$(cat /sys/class/power_supply/BAT1/capacity)"
-	printf "   %s" "$get_capacity"
+  get_capacity="$(cat /sys/class/power_supply/BAT1/capacity)"
+  printf "^c$blue^   $get_capacity"
 }
 
 brightness() {
-	printf "  %s " "$(cat /sys/class/backlight/*/brightness)"
+  printf "^c$red^   "
+  printf "^c$red^%.0f\n" $(cat /sys/class/backlight/*/brightness)
 }
 
 mem() {
-	printf "  %s" "$(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
+  printf "^c$blue^^b$black^  "
+  printf "^c$blue^ $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
 }
 
 wlan() {
 	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
-	up) printf "wlan:  Connected" ;;
-	down) printf "wlan:  Disconnected" ;;
+	up) printf "^c$black^ ^b$blue^ 󰤨 ^d^%s" " ^c$blue^Connected" ;;
+	down) printf "^c$black^ ^b$blue^ 󰤭 ^d^%s" " ^c$blue^Disconnected" ;;
 	esac
 }
 
 clock() {
-	printf "%s" "$(date '+%H:%M')"
+	printf "^c$black^ ^b$darkblue^  "
+	printf "^c$black^^b$blue^ $(date '+%H:%M')  "
 }
 
 while true; do
 
-	[ $interval = 0 ] || [ $(($interval % 3600)) = 0 ]
-	interval=$((interval + 1))
+  [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
+  interval=$((interval + 1))
 
-	sleep 1 && xsetroot -name  "$(battery) $(brightness) $(cpu) $(mem) $(wlan) $(clock)"
+  sleep 1 && xsetroot -name "$(battery) $(brightness) $(cpu) $(mem) $(wlan) $(clock)"
 done
