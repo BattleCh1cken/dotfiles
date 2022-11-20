c339e728b6155648a424531742bb08bb45caca91
BattleCh1cken <BattleCh1cken@Larkov.de>
2022-11-19 22:04:16 -0500

    feat: boxie host

--- hosts/boxie/default.nix
+++ hosts/boxie/default.nix
@@ -1,1 +1,71 @@
+{ pkgs, config, lib, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
+  ## Modules
+  modules = {
+    term.kitty.enable = true;
+    desktop = {
+      #dwm.enable = true;
+      apps.steam.enable = true;
+      apps.thunar.enable = true;
+    };
+    shell.zsh.enable = true;
+    shell.git.enable = true;
+    editors.neovim.enable = true;
+    hardware = {
+      audio.enable = true;
+    };
+    theme.active = "catppuccin";
+
+  };
+
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
+  services.gnome.gnome-keyring.enable = true;
+  environment.systemPackages = with pkgs;[
+    #misc
+    libsecret
+
+    #Apps
+    firefox
+    mailspring
+    vlc
+    gimp
+    obsidian
+    discord-canary
+    google-chrome
+    vscode.fhs
+    #prismlauncher
+    #Command line utils
+    gotop
+    htop
+    cava
+    cmus
+    ranger
+    protonup
+    killall
+    wget
+    cmatrix
+    fd
+    xclip
+    neofetch
+  ];
+
+  networking.networkmanager.enable = true;
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+}
