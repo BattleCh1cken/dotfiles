e86e21a7301c626a24b4bb054a2a34379b8d9a99
BattleCh1cken <BattleCh1cken@Larkov.de>
2022-12-02 11:21:40 -0500

    feat:ncmpcpp

--- modules/hardware/audio.nix
+++ modules/hardware/audio.nix
@@ -3,7 +3,8 @@
 with lib;
 with lib.my;
 let cfg = config.modules.hardware.audio;
-in {
+in
+{
   options.modules.hardware.audio = {
     enable = mkBoolOpt false;
   };
@@ -18,24 +19,23 @@ in {
 
     security.rtkit.enable = true;
 
-    environment.systemPackages = with pkgs; [
-      easyeffects
-    ];
 
     # HACK Prevents ~/.esd_auth files by disabling the esound protocol module
     #      for pulseaudio, which I likely don't need. Is there a better way?
-    hardware.pulseaudio.configFile =
+    /*
+      hardware.pulseaudio.configFile =
       let inherit (pkgs) runCommand pulseaudio;
-        paConfigFile =
-          runCommand "disablePulseaudioEsoundModule"
-            { buildInputs = [ pulseaudio ]; } ''
-            mkdir "$out"
-            cp ${pulseaudio}/etc/pulse/default.pa "$out/default.pa"
-            sed -i -e 's|load-module module-esound-protocol-unix|# ...|' "$out/default.pa"
-          '';
+      paConfigFile =
+      runCommand "disablePulseaudioEsoundModule"
+      { buildInputs = [ pulseaudio ]; } ''
+      mkdir "$out"
+      cp ${pulseaudio}/etc/pulse/default.pa "$out/default.pa"
+      sed -i -e 's|load-module module-esound-protocol-unix|# ...|' "$out/default.pa"
+      '';
       in
       mkIf config.hardware.pulseaudio.enable
-        "${paConfigFile}/default.pa";
+      "${paConfigFile}/default.pa";
+    */
 
     user.extraGroups = [ "audio" ];
   };
