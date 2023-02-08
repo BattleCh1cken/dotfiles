650e77ed8969f9281a74487635276551423faadf
BattleCh1cken <BattleCh1cken@Larkov.de>
2023-02-08 07:39:10 -0500

    fix: nvidia hyprland (it doesn't actually work)

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -3,16 +3,30 @@
 with lib;
 with lib.my;
 let cfg = config.modules.hardware.nvidia;
-in {
+in
+{
   options.modules.hardware.nvidia = {
     enable = mkBoolOpt false;
   };
 
   config = mkIf cfg.enable {
-    hardware.opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
+
+    hardware = {
+      nvidia = {
+        open = true;
+        powerManagement.enable = true;
+        modesetting.enable = true;
+      };
+      opengl = {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true;
+        extraPackages = with pkgs; [
+          vaapiVdpau
+          libvdpau-va-gl
+          nvidia-vaapi-driver
+        ];
+      };
     };
 
     services.xserver.videoDrivers = [ "nvidia" ];
