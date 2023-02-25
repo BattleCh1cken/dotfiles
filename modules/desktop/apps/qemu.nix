892c6858943570b5d86ee8ecb68ff4826d1fdeef
BattleCh1cken <BattleCh1cken@Larkov.de>
2023-02-25 11:55:40 -0500

    feat: qemu module

---
+++ modules/desktop/apps/qemu.nix
@@ -0,0 +1,22 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let
+  cfg = config.modules.desktop.apps.qemu;
+in
+{
+  options.modules.desktop.apps.qemu = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      qemu
+      virt-manager
+    ];
+
+    virtualisation.libvirtd.enable = true;
+
+  };
+}
