b62ad250efbf491e0d9ac5f526ff77df9033173b
BattleCh1cken <BattleCh1cken@larkov.de>
2023-07-26 20:14:21 -0400

    Feat: neovim module

---
+++ modules/editors/neovim/default.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  inputs,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.modules.editors.neovim;
+in {
+  options.modules.editors.neovim = {
+    enable = mkEnableOption "neovim";
+  };
+
+  config = mkIf cfg.enable {
+    nixpkgs.overlays = [
+      inputs.neovim-nightly-overlay.overlay
+    ];
+    environment.systemPackages = with pkgs; [
+      lazygit
+      ripgrep
+      neovim
+    ];
+  };
+}
