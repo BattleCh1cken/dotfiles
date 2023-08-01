19282918ecf1e897050c6fb82c9e81aa217e8060
BattleCh1cken <BattleCh1cken@larkov.de>
2023-08-01 09:35:45 -0400

    Feat: added new binary cache

--- modules/nix-config.nix
+++ modules/nix-config.nix
@@ -1,9 +1,8 @@
-{
-  config,
-  lib,
-  inputs,
-  pkgs,
-  ...
+{ config
+, lib
+, inputs
+, pkgs
+, ...
 }: {
   # Configure nix and nixpkgs
   environment.variables.NIXPKGS_ALLOW_UNFREE = "1";
@@ -16,10 +15,12 @@
       substituters = [
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
+        "https://anyrun.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       ];
       auto-optimise-store = true;
     };
