387f40f07ba3d4ff9693c5ccaa17a32b4dbb6abb
BattleCh1cken <BattleCh1cken@larkov.de>
2024-05-08 21:22:02 -0400

    Update: nvim

--- modules/editors/neovim/config/lua/core/plugins/bufferline.lua
+++ modules/editors/neovim/config/lua/core/plugins/bufferline.lua
@@ -3,20 +3,6 @@ return {
   dependencies = { "kyazdani42/nvim-web-devicons", lazy = true },
   config = function()
     local bufferline = require('bufferline')
-    bufferline.setup {
-      options = {
-        style_preset = bufferline.style_preset.minimal, -- or bufferline.style_preset.minimal,
-      },
-      highlights = {
-        background = {
-          fg = '#ffffff',
-        },
-        buffer_selected = {
-          fg = '#33b1ff',
-        },
-
-
-      },
-    }
+    bufferline.setup {}
   end
 }
