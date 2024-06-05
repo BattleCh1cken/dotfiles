4b024ead27839508d73ab80b40086f92f0682b27
BattleCh1cken <BattleCh1cken@larkov.de>
2024-06-05 14:25:45 -0400

    Feat: Kitty is catppuccin now

--- modules/desktop/term/kitty.nix
+++ modules/desktop/term/kitty.nix
@@ -26,64 +26,81 @@ in
           window_padding_width 0
           enable_audio_bell no
           hide_window_decorations yes
-          globinclude extra/*.conf
           background_opacity 1.0
           window_padding_width 2
 
-          # oxocarbon theme ported to kitty, https://github.com/hbjydev/oxocarbon-kitty
-          # originally made by nyoom, https://github.com/nyoom-engineering/oxocarbon
+          # Catppuccin theme
 
-          foreground #dde1e6
-          background #161616
-          selection_foreground #f2f4f8
-          selection_background #525252
-
-          cursor #f2f4f8
-          cursor_text_color #393939
-
-          url_color #ee5396
-          url_style single
-
-          active_border_color #ee5396
-          inactive_border_color #ff7eb6
-
-          bell_border_color #ee5396
-
+          # The basic colors
+          foreground              #CAD3F5
+          background              #24273A
+          selection_foreground    #24273A
+          selection_background    #F4DBD6
+          
+          # Cursor colors
+          cursor                  #F4DBD6
+          cursor_text_color       #24273A
+          
+          # URL underline color when hovering with mouse
+          url_color               #F4DBD6
+          
+          # Kitty window border colors
+          active_border_color     #B7BDF8
+          inactive_border_color   #6E738D
+          bell_border_color       #EED49F
+          
+          # OS Window titlebar colors
           wayland_titlebar_color system
           macos_titlebar_color system
-
-          active_tab_foreground #161616
-          active_tab_background #ee5396
-          inactive_tab_foreground #dde1e6
-          inactive_tab_background #393939
-          tab_bar_background #161616
-
-          color0 #262626
-          color8 #393939
-
-          color1 #ff7eb6
-          color9 #ff7eb6
-
-          color2  #42be65
-          color10 #42be65
-
-          color3  #82cfff
-          color11 #82cfff
-
-          color4  #33b1ff
-          color12 #33b1ff
-
-          color5  #ee5396
-          color13 #ee5396
-
-          color6  #3ddbd9
-          color14 #3ddbd9
-
-          color7  #dde1e6
-          color15 #ffffff
-
-          # vim:ft=conf
-
+          
+          # Tab bar colors
+          active_tab_foreground   #181926
+          active_tab_background   #C6A0F6
+          inactive_tab_foreground #CAD3F5
+          inactive_tab_background #1E2030
+          tab_bar_background      #181926
+          
+          # Colors for marks (marked text in the terminal)
+          mark1_foreground #24273A
+          mark1_background #B7BDF8
+          mark2_foreground #24273A
+          mark2_background #C6A0F6
+          mark3_foreground #24273A
+          mark3_background #7DC4E4
+          
+          # The 16 terminal colors
+          
+          # black
+          color0 #494D64
+          color8 #5B6078
+          
+          # red
+          color1 #ED8796
+          color9 #ED8796
+          
+          # green
+          color2  #A6DA95
+          color10 #A6DA95
+          
+          # yellow
+          color3  #EED49F
+          color11 #EED49F
+          
+          # blue
+          color4  #8AADF4
+          color12 #8AADF4
+          
+          # magenta
+          color5  #F5BDE6
+          color13 #F5BDE6
+          
+          # cyan
+          color6  #8BD5CA
+          color14 #8BD5CA
+          
+          # white
+          color7  #B8C0E0
+          color15 #A5ADCB
         '';
       };
 
