362b11463f6c3a082dc90dce30ab5934ed907998
battlech1cken <battlech1cken@proton.me>
2026-08-02 17:07:47 -0400

    fix it

--- .config/nvim/lua/core/plugins/treesitter.lua
+++ .config/nvim/lua/core/plugins/treesitter.lua
@@ -2,10 +2,6 @@ return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
   build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter').install { 'rust', 'javascript', 'zig' }
-
-  end,
 }
 
 -- return {
